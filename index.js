807b1de6f3e3735564784288efd9004acb733c5c
Sujan Kumar Roy <sujanroy63836@gmail.com>
2026-02-01 10:07:48 +0530

    form

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ import express from 'express'
 const app = express()
 const PORT = 3333
 
-app.use(express.static('public'))
-app.use(express.static('images'))
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req, res) => {
 	res.send('hello express')
+})
+
+app.post('/form', (req, res) => {
+	
+	console.log(req.body)
+	res.send('form recived')
 })
 
 app.listen(3333, () => {
