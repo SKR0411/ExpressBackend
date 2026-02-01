03ad722ee0171f95796e5d9e42c881767bb1760a
Sujan Kumar Roy <sujanroy63836@gmail.com>
2026-02-01 09:15:49 +0530

    quick fix

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from 'express'
 import router from './router.js'
-import ejs from 'ejs';
 
 const app = express()
 const PORT = 3333
 
 // set ejs as view engine 
-app.set('view engine', ejs)
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
 	const userName = 'sujan kumar roy'
